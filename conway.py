ae89ae21bbf15fbd8b8c628cfc93cd29b903bec0
ShreestinaT <shreestina@equatoranalytics.com>
2022-10-03 12:02:19 +0800

    Add a function print_board

--- conway.py
+++ conway.py
@@ -9,6 +9,14 @@ board = [
 
 def run():
     print(board)
+    print_board(board)
+
+def print_board(board1):
+    for row in board1:
+        for x in row:
+            print(x, end=' ')
+    pass
+
 
 
 if __name__ == '__main__':
