58f633f61ad6d93a075eb9ef59d4338d7c3c5b0b
ShreestinaT <shreestina@equatoranalytics.com>
2022-10-04 14:26:10 +0800

    Started border

--- src/conway.py
+++ src/conway.py
@@ -1,35 +1,31 @@
 import numpy as np
 
 board = [
-    [0, 0, 0, 0, 0, ],
-    [0, 0, 0, 0, 0, ],
-    [0, 0, 0, 0, 0, ],
+    [1, 0, 0, 0, 1, ],
+    [0, 0, 0, 1, 0, ],
+    [0, 0, 1, 0, 0, ],
     [0, 1, 0, 0, 0, ],
-    [1, 0, 0, 0, 0, ],
+    [1, 0, 0, 0, 1, ],
 ]
 
 
 def run():
-    print(board)
-    #print_board(board)
     print_board_alternate(board)
-
-
-def print_board(board1):
-    arr = np.array(board1)
-    print(arr)
-    arr = arr.reshape(5, 5)
-    print(arr)
-    print(str(arr).replace(" [", "").replace("[", "").replace("]", ""))
 
 
 def print_board_alternate(board: list[list[int]]):
     """Another way of printing the board
     """
-    for row in board:
+    length= len(board[0])
+    for row_index, row in enumerate(board):
+        if row_index == 0 :
+            print('+')
         for item in row:
             print(str(item), end=' ')
+        if  row_index== length-1:
+            print('+')
         print()
+
 
 
 
