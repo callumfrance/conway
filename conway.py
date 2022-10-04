88ab0364ea430ed534b8eb66851289a169bc0eb0
Callum France <france.callum@gmail.com>
2022-10-04 11:25:45 +0800

    alternate print_board

--- conway.py
+++ conway.py
@@ -13,12 +13,22 @@ def run():
     print(board)
     print_board(board)
 
+
 def print_board(board1):
     arr = np.array(board1)
-    arr = arr.reshape(5,5)
-    print(str(arr).replace(" [","").replace("[","").replace("]",""))
-    pass
+    print(arr)
+    arr = arr.reshape(5, 5)
+    print(arr)
+    print(str(arr).replace(" [", "").replace("[", "").replace("]", ""))
 
+
+def print_board_alternate(board: list[list[int]]):
+    """Another way of printing the board
+    """
+    for row in board1:
+        for item in row:
+            print(str(item), end=' ')
+        print()
 
 
 if __name__ == '__main__':
