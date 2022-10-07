d2fa4624352cccec0af2637896cefc9ee78e11e6
ShreestinaT <shreestina@equatoranalytics.com>
2022-10-07 12:41:09 +0800

    Added code to overpopulation

--- src/overpopulation.py
+++ src/overpopulation.py
@@ -2,15 +2,15 @@ from cell import get_neighbour_tally
 
 
 def check_overpopulation(board: list[list[int]], x: int, y: int) -> bool:
-    """Any live cell with more than three live neighbours dies, as if by overpopulation.
-    """
-    pass
+    """Any live cell with more than three live neighbours dies,
+    as if by overpopulation."""
+    return get_neighbour_tally(board, x, y) > 3
 
 
 if __name__ == "__main__":
     demo_board_a = [
         [0, 1, 0],
-        [1, 0, 0],
+        [1, 0, 1],
         [0, 1, 0],
     ]
     print(check_overpopulation(demo_board_a, 1, 1))
