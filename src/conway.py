863f017d69a9272a1e0aeb3e536ae26261228753
ShreestinaT <shreestina@equatoranalytics.com>
2022-10-07 12:26:12 +0800

    Added borders

--- src/conway.py
+++ src/conway.py
@@ -10,9 +10,14 @@ def print_board_alternate(board: list[list[int]]):
     length = len(board[0])
     for row_index, row in enumerate(board):
         if row_index == 0:
-            print("+")
+            print("+ - - - - - +")
+        if row_index <= 4:
+            print("|", end=" ")
         for item in row:
             print(str(item), end=" ")
+        if row_index <= 4:
+            print("|", end=" ")
         if row_index == length - 1:
-            print("+")
+            print()
+            print("+ - - - - - +")
         print()
