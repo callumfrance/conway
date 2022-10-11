3d17062a6f1f46fd761f4e7249c6f2606fe598e1
Callum France <france.callum@gmail.com>
2022-10-11 11:32:41 +0800

    missing test file

---
+++ src/test_underpopulation.py
@@ -0,0 +1,18 @@
+import pytest
+
+from .overpopulation import check_overpopulation
+
+demo_board_a = None  # TODO write passing test board
+
+demo_board_b = None  # TODO write failing test board
+
+
+@pytest.mark.parametrize(
+    "input_board, input_x, input_y, expected",
+    [
+        # TODO write test conditions
+    ],
+)
+def test_overpopulation(input_board, input_x, input_y, expected):
+    # TODO write test
+    pass
