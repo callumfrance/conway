6bc7494257c76006667d6fa22477f619b51ec2c6
Callum France <france.callum@gmail.com>
2022-10-11 10:59:20 +0800

    future work template

--- src/underpopulation.py
+++ src/underpopulation.py
@@ -9,16 +9,7 @@ def check_underpopulation(board: list[list[int]], x: int, y: int) -> bool:
 
 
 if __name__ == "__main__":
-    demo_board_a = [
-        [0, 0, 0],
-        [1, 0, 0],
-        [0, 0, 0],
-    ]
+
     print(check_underpopulation(demo_board_a, 1, 1))
 
-    demo_board_b = [
-        [0, 1, 0],
-        [1, 0, 0],
-        [0, 1, 0],
-    ]
     print(check_underpopulation(demo_board_b, 1, 1))
