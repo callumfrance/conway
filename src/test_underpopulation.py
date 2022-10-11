fe2d7502fc885652f4028a681e0a903946f8d208
ShreestinaT <shreestina@equatoranalytics.com>
2022-10-11 15:37:11 +0800

    Changes made to test_underpopulation and underpopulation

--- src/test_underpopulation.py
+++ src/test_underpopulation.py
@@ -1,18 +1,28 @@
 import pytest
 
-from .overpopulation import check_overpopulation
+from .underpopulation import check_underpopulation
 
-demo_board_a = None  # TODO write passing test board
+demo_board_a = [
+    [0, 1, 0],
+    [0, 0, 1],
+    [0, 1, 0],
+]
 
-demo_board_b = None  # TODO write failing test board
+demo_board_b = [
+    [0, 0, 0],
+    [0, 1, 0],
+    [0, 1, 0],
+]
 
 
 @pytest.mark.parametrize(
     "input_board, input_x, input_y, expected",
     [
-        # TODO write test conditions
+        (demo_board_a, 1, 1, False),
+        (demo_board_a, 0, 0, True),
+        (demo_board_b, 0, 1, True),
+        (demo_board_b, 1, 0, False),
     ],
 )
-def test_overpopulation(input_board, input_x, input_y, expected):
-    # TODO write test
-    pass
+def test_underpopulation(input_board, input_x, input_y, expected):
+    assert check_underpopulation(input_board, input_x, input_y) == expected
