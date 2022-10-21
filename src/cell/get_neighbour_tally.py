8b80c3719e01174b5a2d520de2bb487a8a02d093
Callum France <france.callum@gmail.com>
2022-10-21 16:23:20 +0800

    fixed

--- src/cell/get_neighbour_tally.py
+++ src/cell/get_neighbour_tally.py
@@ -5,25 +5,32 @@ from ..board import board
 
 def _get_neighbours(board: list[list[int]], x: int, y: int) -> list[list[int]]:
     neighbours = list()
-    upper_boundaries = (len(board), len(board[0]))
+    upper_boundaries = (len(board) - 1, len(board[0]) - 1)
 
     def add_to_neighbours(lat, lng):
-        return neighbours.append(board[lat][lng])
+        return neighbours.append(board[x + lat][y + lng])
 
     if x != 0:
         if y != 0:
             add_to_neighbours(-1, -1)
+
         add_to_neighbours(-1, 0)
+
         if y != upper_boundaries[0]:
             add_to_neighbours(-1, 1)
+
     if x != upper_boundaries[1]:
         if y != 0:
             add_to_neighbours(1, -1)
+
         add_to_neighbours(1, 0)
+
         if y != upper_boundaries[0]:
             add_to_neighbours(1, 1)
+
     if y != 0:
         add_to_neighbours(0, -1)
+
     if y != upper_boundaries[0]:
         add_to_neighbours(0, 1)
 
@@ -39,7 +46,7 @@ def get_neighbour_tally(board: list[list[int]], x: int, y: int) -> int:
 
     # For more information on functools.reduce() -
     #    @See https://docs.python.org/3/library/functools.html#functools.reduce
-    return reduce(sum_neighbours, _get_neighbours(board, x, y))
+    return reduce(sum_neighbours, _get_neighbours(board, x, y), 0)
 
 
 if __name__ == "__main__":
--- src/cell/test_cell.py
+++ src/cell/test_cell.py
@@ -53,15 +53,16 @@ def test_cell():
 def test_cell_alive(input_board, input_x, input_y, expected):
     assert process_alive(input_board, input_x, input_y) == expected
 
+
 @pytest.mark.parametrize(
     "input_board, input_x, input_y, expected",
     [
-        (demo_board_a, 1, 1, 1),
-        (demo_board_a, 1, 2, 1),
+        (demo_board_a, 1, 1, 0),
+        (demo_board_a, 1, 2, 0),
         (demo_board_a, 0, 0, 0),
-        (demo_board_b, 1, 1, 0),
+        (demo_board_b, 0, 0, 1),
         (demo_board_c, 1, 0, 0),
     ],
 )
-def test_cell_alive(input_board, input_x, input_y, expected):
+def test_cell_dead(input_board, input_x, input_y, expected):
     assert process_dead(input_board, input_x, input_y) == expected
